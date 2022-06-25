6d0981eda4846f68562c2da4ef3192034a2a2679
Navjot Dhanawat <navjotdhanawat@gmail.com>
2022-06-25 13:53:04 -0400

    initial

---
+++ src/Containers/ExampleContainer.tsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import {
+  View,
+  ActivityIndicator,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native'
+import { useDispatch } from 'react-redux'
+import { useTranslation } from 'react-i18next'
+import { DigitalGauge } from '@/Components'
+import { useTheme } from '@/Hooks'
+import { useLazyFetchOneQuery } from '@/Services/modules/users'
+import { changeTheme, ThemeState } from '@/Store/Theme'
+
+const ExampleContainer = () => {
+  const { t } = useTranslation()
+  const { Common, Fonts, Gutters, Layout } = useTheme()
+  const dispatch = useDispatch()
+
+  const [userId, setUserId] = useState('9')
+  const [fetchOne, { data, isSuccess, isLoading, isFetching, error }] =
+    useLazyFetchOneQuery()
+
+  useEffect(() => {
+    fetchOne(userId)
+  }, [fetchOne, userId])
+
+  const onChangeTheme = ({ theme, darkMode }: Partial<ThemeState>) => {
+    dispatch(changeTheme({ theme, darkMode }))
+  }
+
+  return (
+    <ScrollView
+      style={Layout.fill}
+      contentContainerStyle={[
+        Layout.fill,
+        Layout.colCenter,
+        Gutters.smallHPadding,
+      ]}
+    >
+      <DigitalGauge value={32.22} unit={'V'} />
+    </ScrollView>
+  )
+}
+
+export default ExampleContainer
